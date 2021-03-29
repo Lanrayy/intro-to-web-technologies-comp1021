f309d42d7d4e9316f5a9bb17135fbe65c2eee5f9
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2021-03-30 00:00:41 +0100

    Privacy notice pop-up completed.

--- Coursework2/scripts/script.js
+++ Coursework2/scripts/script.js
@@ -17,6 +17,8 @@ $(function(){
     {
         // if it has been accepted, hide the privacy notice
         $(".privacy-notice-container").hide();
+        $(".privacy-notice-pop-up").hide();
+        $(".privacy-notice-pop-up-overlay").hide();
     }
     else
     {
@@ -26,19 +28,52 @@ $(function(){
     //when the close button is pressed
     //hide the privacy notice
 
+    $(".advanced-button").on("click", function(){
+        $(".privacy-notice-pop-up").css({"display":"inherit"}).delay(1000).fadeIn(5000);
+        $(".privacy-notice-pop-up-overlay").css({"display":"flex"});
+
+    })
+
+    //if the accept button is clicked hide the privacy notice
+    $(".accept-button").on("click", function(){
+        $(".privacy-notice-container").hide();
+        $(".privacy-notice-pop-up").hide();
+        notice = "true";
+        sessionStorage.setItem("notice", notice);
+    })
+
+    //When the close button is clicked
     $("#close-button").on("click", function(){
         $(".privacy-notice-container").hide();
         notice = "false";
         sessionStorage.setItem("notice", notice);
     })
 
-    //if the accept button is clicked hide the privacy notice
+    //When the privacy popup close button is clicked, hide the privacy popup
+    $("#privacy-close-button").on("click", function(){
+        $(".privacy-notice-pop-up").hide();
+        $(".privacy-notice-pop-up-overlay").hide();
+    })
 
-    $(".accept-button").on("click", function(){
+    //When the save buton is clicked, hide the privacy popup and overlay
+    $(".save-button").on("click", function(){
+        $(".privacy-notice-pop-up").hide();
+        $(".privacy-notice-pop-up-overlay").hide();
         $(".privacy-notice-container").hide();
         notice = "true";
         sessionStorage.setItem("notice", notice);
     })
+
+
+
+
+
+
+
+
+
+
+
 
 
     //Geolocation
@@ -48,27 +83,24 @@ $(function(){
     function success(position){
 
         // fetch the coordinates
-
         var mylat = position.coords.latitude;
         var mylong = position.coords.longitude;
 
         $("#current-location").html("Your current location is Latitude: " +  mylat + ", Longitude: " + mylong);
 
-        //$("#current-location").html("Your current location is" + mylat + mylong);
-
     }
 
     function failure(){
-        $("#current-location").html("Your current location is: unable to get current locaation");
-
+        $("#current-location").html("Your current location is: Unable to get current location! Please enable location services!");
     }
 
     //$("#overlay").hide();
 
     //PORTFOLIO PAGE OVERLAY
-
    $(".overlay").hide();
     var Id;
+
+    //when a user hovers over a project
    $(".project-container").on("mouseenter", function(){
        Id = $(this).attr("id");
        //alert("#project-title-"+Id);
@@ -79,6 +111,7 @@ $(function(){
         $("#project-image-"+Id).css({"width":"530px", "height":"380px"});
     });
 
+    //When the mouse leaves the project
     $(".project-container").on("mouseleave", function(){
         //alert(Id)
         $(".overlay").fadeOut(200);
@@ -86,8 +119,7 @@ $(function(){
         $(".project-description").css({"text-decoration":"none", "font-size":"18px"});
         $(".project-image").css({"width":"500px", "height":"350px"});
         $("#overlay"+Id).css({"width":"500px", "height":"350px"});
-
-    })
+    });
 
 
 });
